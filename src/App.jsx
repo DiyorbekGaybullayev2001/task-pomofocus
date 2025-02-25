9b55f8612748137f85f8b0f4f6a49ba579beecff
Diyorbek G'aybullayev <gaybullayevdiyorbek2001@gmail.com>
2025-02-25 17:37:07 +0500

    add file

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='text-[30px] text-amber-600'>salom</div>
+    </>
+  )
+}
+
+export default App
